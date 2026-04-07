8b8bdce9ada0fdb5becd2f7d121f8569ac688fec
Edison <premusic314@gmail.com>
2026-04-07 10:58:41 -0500

    clase 07-04-2026

---
+++ modulo1/python_basics/mp/01_hello_world_mp.py
@@ -0,0 +1,11 @@
+print("Bienvenido a StaticsFootball")
+
+edad = 21
+nombre = "Edison Ludeña"
+print(f"El jugador {nombre} tiene {edad} años.")
+
+print("formativas", "primer equipo", "sub-17")
+print("Nombre jugador:", nombre, "edad:", edad)
+
+telefono = 12121211212
+print(nombre, edad, telefono, sep=", ")
